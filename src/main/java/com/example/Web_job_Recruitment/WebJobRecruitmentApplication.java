be606f996c9bae8c70cc8a43d22bea4d1b97a67f
deebeehoang <124672123+deebeehoang@users.noreply.github.com>
2026-02-02 19:20:57 +0700

    Chore/setup readme gitignore (#2)
    * Add README and .gitignore
    
    * xay dung cau truc spring boot
    
    ---------
    
    Co-authored-by: HoangNguyen004 <124672123+HoangNguyen004@users.noreply.github.com>

---
+++ src/main/java/com/example/Web_job_Recruitment/WebJobRecruitmentApplication.java
@@ -0,0 +1,13 @@
+package com.example.Web_job_Recruitment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebJobRecruitmentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebJobRecruitmentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Web_job_Recruitment/WebJobRecruitmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Web_job_Recruitment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebJobRecruitmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
